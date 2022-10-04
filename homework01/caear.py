c2cf319192f19f497a374e391191d0d8244ddf9c
DarkDan <82237106+DarkDan81@users.noreply.github.com>
2022-10-04 23:43:38 +0300

    Add files via upload

--- homework01/caear.py
+++ homework01/caear.py
@@ -1,15 +1,7 @@
-def encrypt_vigenere(plaintext: str, keyword: str) -> str:
+def encrypt_caesar(plaintext: str, shift: int = 3) -> str:
     ciphertext = ""
-    if len(plaintext) > len(keyword):
-        x = len(plaintext) // len(keyword)
-        keyword *= x + 2
     for i in range(len(plaintext)):
-        shift = ord(keyword[i])
         char = plaintext[i]
-        if shift >= 65 and shift <= 90:
-            shift -= 65
-        elif shift >= 97 and shift <= 122:
-            shift -= 97
         if ord(char) >= 65 and ord(char) <= 90:
             char = chr(ord(char) + shift)
             if ord(char) > 90:
@@ -19,21 +11,14 @@ def encrypt_vigenere(plaintext: str, keyword: str) -> str:
             if ord(char) > 122:
                 char = chr(ord(char) - 26)
         ciphertext += char
+
     return ciphertext
 
 
-def decrypt_vigenere(ciphertext: str, keyword: str) -> str:
+def decrypt_caesar(ciphertext: str, shift: int = 3) -> str:
     plaintext = ""
-    if len(ciphertext) > len(keyword):
-        x = len(ciphertext) // len(keyword)
-        keyword *= x + 2
     for i in range(len(ciphertext)):
-        shift = ord(keyword[i])
         char = ciphertext[i]
-        if shift >= 65 and shift <= 90:
-            shift -= 65
-        elif shift >= 97 and shift <= 122:
-            shift -= 97
         if ord(char) >= 65 and ord(char) <= 90:
             char = chr(ord(char) - shift)
             if ord(char) < 65:
@@ -43,4 +28,5 @@ def decrypt_vigenere(ciphertext: str, keyword: str) -> str:
             if ord(char) < 97:
                 char = chr(ord(char) + 26)
         plaintext += char
+
     return plaintext
--- homework01/rsa.py
+++ homework01/rsa.py
@@ -5,7 +5,6 @@ import typing as tp
 def is_prime(n: int) -> bool:
     """
     Tests to see if a number is prime.
-
     >>> is_prime(2)
     True
     >>> is_prime(11)
@@ -13,33 +12,37 @@ def is_prime(n: int) -> bool:
     >>> is_prime(8)
     False
     """
-    # PUT YOUR CODE HERE
-    pass
+    return n > 1 and all(n % i != 0 for i in range(2, int(n ** 0.5) + 1))
 
 
 def gcd(a: int, b: int) -> int:
     """
     Euclid's algorithm for determining the greatest common divisor.
-
     >>> gcd(12, 15)
     3
     >>> gcd(3, 7)
     1
     """
-    # PUT YOUR CODE HERE
-    pass
+    if a == 0:
+        return b
+    return gcd(b % a, a)
 
 
 def multiplicative_inverse(e: int, phi: int) -> int:
     """
     Euclid's extended algorithm for finding the multiplicative
     inverse of two numbers.
-
     >>> multiplicative_inverse(7, 40)
     23
     """
-    # PUT YOUR CODE HERE
-    pass
+
+    def gcd_extended(a, b):
+        if a == 0:
+            return (b, 0, 1)
+        else:
+            div, x, y = gcd_extended(b % a, a)
+        return (div, y - (b // a) * x, x)
+
 
 
 def generate_keypair(p: int, q: int) -> tp.Tuple[tp.Tuple[int, int], tp.Tuple[int, int]]:
@@ -49,10 +52,10 @@ def generate_keypair(p: int, q: int) -> tp.Tuple[tp.Tuple[int, int], tp.Tuple[in
         raise ValueError("p and q cannot be equal")
 
     # n = pq
-    # PUT YOUR CODE HERE
+    n = p * q
 
     # phi = (p-1)(q-1)
-    # PUT YOUR CODE HERE
+    phi = (p - 1) * (q - 1)
 
     # Choose an integer e such that e and phi(n) are coprime
     e = random.randrange(1, phi)
