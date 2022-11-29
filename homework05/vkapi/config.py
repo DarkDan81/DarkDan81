8290602ce19d76802cca7dc89b98707951d7d4f4
DarkDan <82237106+DarkDan81@users.noreply.github.com>
2022-11-29 22:16:38 +0300

    Add files via upload

--- homework05/vkapi/config.py
+++ homework05/vkapi/config.py
@@ -2,6 +2,6 @@
 
 VK_CONFIG = {
     "domain": "https://api.vk.com/method",
-    "access_token": "",
-    "version": "5.126",
+    "access_token": "vk1.a.I-D9tJuCdYXbfugvuW5Su-THkMwaxnefhib6LXqDvzlscC8QFwnPG4z38eete8emhFRNmuQlhcAyHu-uisMbdClP6eWjXHZPMz_tDJy-Mz5JOnQeqe0wLr9YV0whvk7u73CUB-T9X5EwUVVY6gbFUfDhNS8jv_J2G1AKr2ZTAGB726HyprxixcE9jWzZY2su",
+    "version": "5.131",
 }
